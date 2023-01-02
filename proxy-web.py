a561a06b8d4d020cd67b64a5b3543c472b37d721
dytttf <dytttf@foxmail.com>
2023-01-03 02:03:47 +0800

    增加日志；提升稳定性

--- proxy-web.py
+++ proxy-web.py
@@ -1,18 +1,46 @@
 # coding:utf8
+import os
+import re
+import sys
 import time
 from socket import *
 import select
 import argparse
-import traceback
 from concurrent.futures import ThreadPoolExecutor
+import logging
+
+
+def get_logger(filename):
+    """"""
+    # 取文件名 不带后缀
+    filename = filename.split(os.sep)[-1].split(".")[0]
+    _logger = logging.getLogger(filename)
+    # 标准输出流
+    std = logging.StreamHandler(sys.stdout)
+    _logger.addHandler(std)
+
+    # 定制标准输出日志格式
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(filename)s] [%(lineno)d] - %(message)s"
+    )
+    std.setFormatter(formatter)
+    _logger.setLevel(logging.DEBUG)
+    return _logger
+
+
+logger = get_logger(__file__)
 
 
 class SocketTunnelServer:
     heartbeat_msg = b"Tb;\x0bM|sF*q Ew)\x0cOpSZr-C9!7XKkH\t"
 
-    def create_socket(self, connect_addr=None, bind_addr=None, listen=False):
+    def create_socket(
+        self, connect_addr=None, bind_addr=None, listen=False, reuse=True
+    ):
         socket_obj = socket(AF_INET, SOCK_STREAM)
-        socket_obj.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        socket_obj.settimeout(300)
+        if reuse:
+            socket_obj.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         if connect_addr:
             socket_obj.connect(connect_addr)
         if bind_addr:
@@ -22,26 +50,33 @@ class SocketTunnelServer:
         return socket_obj
 
     def exchange_loop(self, client, remote, heartbeat_socket=None):
-        client_peer = client.getpeername()
-        remote_peer = remote.getpeername()
         chunk_size = 8192
+        client_tag = ""
+        remote_tag = ""
         try:
+            client_tag = re.search("(laddr.*?)>", str(client)).group(1)
+            remote_tag = re.search("(laddr.*?)>", str(remote)).group(1)
+            if "laddr" not in client_tag or "raddr" not in client_tag:
+                raise Exception(f"client connect failed: {client_tag}, {remote_tag}")
+            if "laddr" not in remote_tag or "raddr" not in remote_tag:
+                raise Exception(f"remote connect failed: {client_tag}, {remote_tag}")
+
             # 处理数据
             while 1:
                 recv_data = b""
-                r, w, e = select.select([client, remote], [], [client, remote], 30)
+                r, w, e = select.select([client, remote], [], [client, remote], 20)
                 if e:
                     break
                 if client in r:
                     recv_data = client.recv(chunk_size)
-                    # print(f"client {client_peer}, {remote_peer} recv: {recv_data}")
+                    # logger.debug(f"client {client_tag}, {remote_tag} recv: {recv_data}")
                     send_data = recv_data.replace(self.heartbeat_msg, b"")
                     # 心跳数据是需要抛弃的 不需要响应
                     if send_data:
                         remote.send(send_data)
                 if remote in r:
                     recv_data = remote.recv(chunk_size)
-                    # print(f"remote {client_peer}, {remote_peer} recv: {recv_data}")
+                    # logger.debug(f"remote {client_tag}, {remote_tag} recv: {recv_data}")
                     send_data = recv_data.replace(self.heartbeat_msg, b"")
                     # 心跳数据是需要抛弃的 不需要响应
                     if send_data:
@@ -49,18 +84,35 @@ class SocketTunnelServer:
                 if not r:
                     if heartbeat_socket:
                         # 心跳 检测连接是否还活着
+                        logger.debug(f"heartbeat {client_tag}, {remote_tag}: start")
                         heartbeat_socket.send(self.heartbeat_msg)
                     continue
                 if not recv_data:
+                    # logger.debug(f"no recv_data {client_tag}, {remote_tag}")
                     break
         except Exception as e:
-            traceback.print_exc()
-        client.shutdown()
-        client.close()
-        remote.shutdown()
-        remote.close()
-        print(f"closed {client_peer}, {remote_peer}")
+            logger.exception(e)
+        try:
+            client.close()
+        except Exception as e:
+            logger.exception(e)
+        try:
+            remote.close()
+        except Exception as e:
+            logger.exception(e)
+        # logger.debug(f"closed {client_tag}, {remote_tag}")
         return
+
+    def inte_worker(self, web_addr, socks_addr):
+        while 1:
+            try:
+                web_socket = self.create_socket(connect_addr=web_addr, reuse=False)
+                socks_socket = self.create_socket(connect_addr=socks_addr, reuse=False)
+                # logger.debug("{}, {}".format(web_socket, socks_socket))
+                self.exchange_loop(web_socket, socks_socket, web_socket)
+            except Exception as e:
+                logger.exception(e)
+                time.sleep(1)
 
     def run_inte(self, web_addr="", max_thread=100, **kwargs):
         """
@@ -79,16 +131,16 @@ class SocketTunnelServer:
             web_addr = web_addr.split(":")
             web_addr = (web_addr[0], int(web_addr[1]))
         else:
-            web_addr = ("127.0.0.1", 9999)
+            web_addr = ("", 9999)
         # 内网socks地址
-        socks_addr = ("127.0.0.1", 9011)
-        print("inte restarting")
+        socks_addr = ("", 9011)
+        logger.debug("inte restarting")
 
         pool = ThreadPoolExecutor(max_workers=max_thread)
-        while 1:
-            web_socket = self.create_socket(connect_addr=web_addr)
-            socks_socket = self.create_socket(connect_addr=socks_addr)
-            pool.submit(self.exchange_loop, web_socket, socks_socket, web_socket)
+        for i in range(max_thread):
+            time.sleep(0.3)
+            pool.submit(self.inte_worker, web_addr, socks_addr)
+        pool.shutdown()
 
     def run_web(self, max_thread=100, **kwargs):
         """
@@ -111,21 +163,33 @@ class SocketTunnelServer:
         client_connect_socket = self.create_socket(
             bind_addr=client_connect_addr, listen=True
         )
-        print("web starting")
+        logger.debug("web starting")
         #
         pool = ThreadPoolExecutor(max_workers=max_thread)
-        while 1:
-            # 接收连接
-            proxy_socket, proxy_addr = proxy_connect_socket.accept()
-            client_socket, client_addr = client_connect_socket.accept()
-            pool.submit(self.exchange_loop, client_socket, proxy_socket, proxy_socket)
+        try:
+            while 1:
+                # 接收连接
+                proxy_socket, proxy_addr = proxy_connect_socket.accept()
+                if "raddr" not in str(proxy_socket):
+                    try:
+                        proxy_socket.close()
+                    except Exception as e:
+                        logger.exception(e)
+                    continue
+                client_socket, client_addr = client_connect_socket.accept()
+                pool.submit(
+                    self.exchange_loop, client_socket, proxy_socket, proxy_socket
+                )
+        except Exception as e:
+            pool.shutdown(wait=False, cancel_futures=True)
+            raise e
 
     def serve_forever(self, func, **kwargs):
         while 1:
             try:
                 func(**kwargs)
             except Exception as e:
-                traceback.print_exc()
+                logger.exception(e)
             time.sleep(10)
 
 
